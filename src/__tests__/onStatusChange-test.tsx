a5215f199196c69ee0d81655a862b59eaa1520cc
EugeneTsarenko <jei.tzarenko@gmail.com>
2022-05-20 15:19:56 +0300

    - Add onStatusChange async tests

--- src/__tests__/onStatusChange-test.tsx
+++ src/__tests__/onStatusChange-test.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import * as TestRenderer from 'react-test-renderer';
 import { HelperRouteObject, RouteHelperStatus } from '../types';
+import { MockAsyncGuard } from './utils/mock-async-guard';
 import { MockSyncGuard } from './utils/mock-sync-guard';
 import { RoutesRenderer } from './utils/RoutesRenderer';
 import { wait } from './utils/wait';
@@ -124,6 +125,102 @@ describe('onStatusChange function', () => {
       });
 
       await wait(10);
+      expect(statuses.length).toBe(2);
+
+      statuses.forEach((status, index) => {
+        expect(status).toBe(expectedStatuses[index]);
+      });
+    });
+  });
+
+  describe('with async guards', () => {
+    it('for component with canActivate true from all 2 guards', async () => {
+      let renderer: TestRenderer.ReactTestRenderer;
+
+      const statuses: RouteHelperStatus[] = [];
+      const expectedStatuses = [RouteHelperStatus.Loading, RouteHelperStatus.Loaded];
+      const routes: HelperRouteObject[] = [
+        {
+          path: '/',
+          element: <div>Home</div>,
+          guards: [new MockAsyncGuard(true, guardAsyncTime), new MockAsyncGuard(true, guardAsyncTime)],
+          onStatusChange: (status: RouteHelperStatus) => {
+            statuses.push(status);
+          },
+        },
+      ];
+
+      TestRenderer.act(() => {
+        renderer = TestRenderer.create(
+          <MemoryRouter initialEntries={['/']}>
+            <RoutesRenderer routes={routes} />
+          </MemoryRouter>,
+        );
+      });
+
+      await wait(guardAsyncTime * 2 + 20);
+      expect(statuses.length).toBe(2);
+
+      statuses.forEach((status, index) => {
+        expect(status).toBe(expectedStatuses[index]);
+      });
+    });
+    it('for component with canActivate false from first guard', async () => {
+      let renderer: TestRenderer.ReactTestRenderer;
+
+      const statuses: RouteHelperStatus[] = [];
+      const expectedStatuses = [RouteHelperStatus.Loading, RouteHelperStatus.Failed];
+      const routes: HelperRouteObject[] = [
+        {
+          path: '/',
+          element: <div>Home</div>,
+          guards: [new MockAsyncGuard(false, guardAsyncTime), new MockAsyncGuard(true, guardAsyncTime)],
+          onStatusChange: (status: RouteHelperStatus) => {
+            statuses.push(status);
+          },
+        },
+      ];
+
+      TestRenderer.act(() => {
+        renderer = TestRenderer.create(
+          <MemoryRouter initialEntries={['/']}>
+            <RoutesRenderer routes={routes} />
+          </MemoryRouter>,
+        );
+      });
+
+      await wait(guardAsyncTime + 10);
+      expect(statuses.length).toBe(2);
+
+      statuses.forEach((status, index) => {
+        expect(status).toBe(expectedStatuses[index]);
+      });
+    });
+    it('for component with canActivate false from second guard', async () => {
+      let renderer: TestRenderer.ReactTestRenderer;
+
+      const statuses: RouteHelperStatus[] = [];
+      const expectedStatuses = [RouteHelperStatus.Loading, RouteHelperStatus.Failed];
+      const routes: HelperRouteObject[] = [
+        {
+          path: '/',
+          element: <div>Home</div>,
+          guards: [new MockAsyncGuard(true, guardAsyncTime), new MockAsyncGuard(false, guardAsyncTime)],
+          onStatusChange: (status: RouteHelperStatus) => {
+            statuses.push(status);
+          },
+        },
+      ];
+
+      TestRenderer.act(() => {
+        renderer = TestRenderer.create(
+          <MemoryRouter initialEntries={['/']}>
+            <RoutesRenderer routes={routes} />
+          </MemoryRouter>,
+        );
+      });
+
+      await wait(guardAsyncTime * 2 + 20);
       expect(statuses.length).toBe(2);
 
       statuses.forEach((status, index) => {
