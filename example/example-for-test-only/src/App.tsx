d500333579c782fc17890d26141ac828ec2ac713
eugenetsarenko <eugene@wellyes.ru>
2022-05-24 13:56:59 +0300

    - Add example details

--- example/example-for-test-only/src/App.tsx
+++ example/example-for-test-only/src/App.tsx
@@ -16,29 +16,57 @@ function Home() {
       <h1>Home</h1>
       <nav>
         <Link to="/">Home</Link> |{" "}
-        <Link to="/home22">Home 22</Link>
+        <Link to="/child">Child</Link> |{" "}
+        <Link to="/child/child2">Child 2</Link> |{" "}
+        <Link to="/child/child2/child3">Child 3</Link>
       </nav>
       <Outlet/>
     </div>
   );
 }
 
-function Home2() {
-  // useLoadingNotification(Home, (status: Status) => {
-  //   console.log(status);
-  // });
-  useEffect(() => {
-    console.log('rendered HOME2');
-  }, []);
+function Child() {
+  return (
+    <div>
+      <h1>Child </h1>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/child">Child</Link> |{" "}
+        <Link to="/child/child2">Child 2</Link> |{" "}
+        <Link to="/child/child2/child3">Child 3</Link>
+      </nav>
+      <Outlet/>
+    </div>
+  );
+}
 
+function Child2() {
+  return (
+    <div>
+      <h1>Child 2</h1>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/child">Child</Link> |{" "}
+        <Link to="/child/child2">Child 2</Link> |{" "}
+        <Link to="/child/child2/child3">Child 3</Link>
+      </nav>
+      <Outlet/>
+    </div>
+  );
+}
+
+function Child3() {
 
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Child 3</h1>
       <nav>
         <Link to="/">Home</Link> |{" "}
-        <Link to="/home22">Home 22</Link>
+        <Link to="/child">Child</Link> |{" "}
+        <Link to="/child/child2">Child 2</Link> |{" "}
+        <Link to="/child/child2/child3">Child 3</Link>
       </nav>
+      <Outlet/>
     </div>
   );
 }
@@ -63,18 +91,24 @@ const RoutesWrapper = () => {
       // },
       children: [
         {
-          path: "home22",
-          element: <Home2 />,
+          path: "child",
+          element: <Child />,
           // onStatusChange: (status: Status) => {
           //   console.log("status 2", Status[status]);
           // },
           // guards: [new MockGuard()]
-          // children: [
-          //   {
-          //     path: "nested2",
-          //     element: <Home />
-          //   }
-          // ]
+          children: [
+            {
+              path: "child2",
+              element: <Child2 />,
+              children: [
+                {
+                  path: "child3",
+                  element: <Child3 />
+                }
+              ]
+            }
+          ]
         }
       ]
     },
