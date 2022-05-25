6c59e15dee4a9dd07ad32c49f44d50ae745a834c
eugenetsarenko <eugene@wellyes.ru>
2022-05-25 17:07:06 +0300

    - Add tests to check correct rendering with RouteHelper component

---
+++ src/__tests__/route-helper-component-test.tsx
@@ -0,0 +1,134 @@
+import * as React from 'react';
+import { MemoryRouter, Outlet, Route, Routes } from 'react-router-dom';
+import * as TestRenderer from 'react-test-renderer';
+import { RouteHelper } from '../route-helper';
+import { guardWaitTimeBeforeCheck, mockGuardWorkTime } from './utils/guard-utils';
+import { MockAsyncGuard } from './utils/mock-async-guard';
+import { wait } from './utils/wait';
+
+describe('route helper component', () => {
+  describe('without extra functionality', () => {
+    it('should render route', async () => {
+      let renderer: TestRenderer.ReactTestRenderer;
+
+      TestRenderer.act(() => {
+        renderer = TestRenderer.create(
+          <MemoryRouter initialEntries={['/']}>
+            <Routes>
+              <Route path="/" element={<RouteHelper element={<div>Home</div>} />} />
+            </Routes>
+          </MemoryRouter>,
+        );
+      });
+
+      await wait(1);
+      expect(renderer.toJSON()).toMatchInlineSnapshot(`
+              <div>
+                Home
+              </div>
+          `);
+    });
+
+    it('should render route nested route', async () => {
+      let renderer: TestRenderer.ReactTestRenderer;
+
+      TestRenderer.act(() => {
+        renderer = TestRenderer.create(
+          <MemoryRouter initialEntries={['/child']}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <RouteHelper
+                    element={
+                      <div>
+                        Home <Outlet />
+                      </div>
+                    }
+                  />
+                }
+              >
+                <Route path="child" element={<RouteHelper element={<div>Child</div>} />}></Route>
+              </Route>
+            </Routes>
+          </MemoryRouter>,
+        );
+      });
+
+      await wait(1);
+      expect(renderer.toJSON()).toMatchInlineSnapshot(`
+        <div>
+          Home 
+          <div>
+            Child
+          </div>
+        </div>
+      `);
+    });
+  });
+
+  describe('with guard', () => {
+    it('should render route', async () => {
+      let renderer: TestRenderer.ReactTestRenderer;
+
+      TestRenderer.act(() => {
+        renderer = TestRenderer.create(
+          <MemoryRouter initialEntries={['/']}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <RouteHelper guards={[new MockAsyncGuard(true, mockGuardWorkTime)]} element={<div>Home</div>} />
+                }
+              />
+            </Routes>
+          </MemoryRouter>,
+        );
+      });
+
+      await wait(1);
+      expect(renderer.toJSON()).toMatchInlineSnapshot(`null`);
+
+      await wait(mockGuardWorkTime + guardWaitTimeBeforeCheck);
+
+      expect(renderer.toJSON()).toMatchInlineSnapshot(`
+        <div>
+          Home
+        </div>
+      `);
+    });
+
+    it('should render route nested route', async () => {
+      let renderer: TestRenderer.ReactTestRenderer;
+
+      TestRenderer.act(() => {
+        renderer = TestRenderer.create(
+          <MemoryRouter initialEntries={['/child']}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <RouteHelper
+                    guards={[new MockAsyncGuard(true, mockGuardWorkTime)]}
+                    element={
+                      <div>
+                        Home <Outlet />
+                      </div>
+                    }
+                  />
+                }
+              >
+                <Route path="child" element={<RouteHelper guards={[new MockAsyncGuard(true, mockGuardWorkTime)]} element={<div>Child</div>} />}></Route>
+              </Route>
+            </Routes>
+          </MemoryRouter>,
+        );
+      });
+
+      await wait(1);
+      expect(renderer.toJSON()).toMatchInlineSnapshot(`null`);
+
+      await wait(mockGuardWorkTime * 2 + guardWaitTimeBeforeCheck);
+    });
+  });
+});
