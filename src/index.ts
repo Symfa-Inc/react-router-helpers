e18e1b5b40d663eb37d903947f4fd56d31b23796
EugeneTsarenko <jei.tzarenko@gmail.com>
2022-07-03 14:49:09 +0300

    - Add export from types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { RouteHelper, HelperOutlet } from './route-helper';
-export { useRoutesWithHelper } from './route-helper-wrappers';
-export { useResolver, useGuardStatus, useResolverStatus, useStatus, useLazyStatus, useLazyError } from './hooks';
-export { Guard, RouteHelperStatus, OnStatusChange, Resolver, LazyLoadError } from './types';
+export { RouteHelper, HelperOutlet } from "./route-helper";
+export { useRoutesWithHelper } from "./route-helper-wrappers";
+export { useResolver, useGuardStatus, useResolverStatus, useLazyStatus, useLazyError } from "./hooks";
+export { Guard, RouteHelperStatus, OnStatusChange, Resolver, LazyLoadError, HelperRouteObject } from "./types";
